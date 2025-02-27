c2142eec613a82d2b00fbb8b287fdd85055f7296
Anastasia Fedulova <Fedulova.a.m@bk.ru>
2025-02-27 17:27:56 +0300

    4 Tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -29,14 +29,21 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldSayBuyAnother200(){
+    public void shouldSayBuyMore2() {
         CashbackHackService service = new CashbackHackService();
-
-        int actual = service.remain(800);
-        int expected = 200;
+        int actual = service.remain(900);
+        int expected = 100;
 
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldNotSayBuyMore2(){
+        CashbackHackService service = new CashbackHackService();
 
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
